cb43d6bdec53f04e77e52d1fdf365ee23d5999f7
Nurcahyani-N <nurcahyani4584@gmail.com>
2026-02-12 11:02:32 +0700

    tambah latihan pert3

---
+++ pertemuan-3/latihan-praktikum/latihanlapokoknya.py
@@ -0,0 +1,22 @@
+class Komik:
+    def __init__(self, judul, asal, genre, rate):
+        self.judul = judul
+        self.asal = asal
+        self.genre = genre
+        self.rate = rate
+
+    def review_reader (self):
+        print(f'Komik bergenre {self.genre} dengan judul {self.judul} yang berasal dari {self.asal} diberi rating {self.rate}')
+
+    def ganti_rate (self, rate_baru):
+        self.rate = rate_baru
+        print(f'Rate terbaru dari komik {self.judul} adalah {rate_baru}')
+
+komikSatu = Komik ('Solo Leveling', 'Korea', 'Action', '9.0')
+komikDua = Komik ('Frieren', 'Jepang', 'Fantasi', '9.35')
+komikTiga = Komik ('Omniscient Reader Viewpoint', 'Korea', 'Action', '9.1')
+
+komikSatu.ganti_rate (8.9)
+komikSatu.review_reader()
+komikDua.review_reader()
+komikTiga.review_reader()
